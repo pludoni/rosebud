587406198eb5c9c533206c1a961c25c6886d6f83
Stefan Wienert <info@stefanwienert.de>
2018-12-03 15:32:00 +0100

    Use blank instead of nil/strip
    Also works with Arrays, Objects etc.

--- lib/rosebud/validations/presence_validator.rb
+++ lib/rosebud/validations/presence_validator.rb
@@ -2,10 +2,10 @@ module Rosebud
   module Validations
     class PresenceValidator < Validator
       def validate_param(name, value, options)
-        error!(:presence, param: name) if value.nil? || value.strip == ''
+        error!(:presence, param: name) if value.blank?
       end
     end
-    
+
     Validations.register_validator!(:presence, PresenceValidator)
   end
 end
